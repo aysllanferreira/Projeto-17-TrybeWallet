d33025f9ea69174e6ff1fae434f4eaf40cf7e08c
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-12-08 14:29:03 -0300

    feat: login feito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { saveUser } from '../redux/actions';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+function Login(props) {
+  const [validInput, setValidInput] = useState(true);
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const verifyInputs = (email, senha) => {
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const passLength = 6;
+    const passwordRegex = senha.length >= passLength;
+    const emailTest = emailRegex.test(email);
+    return !!(emailTest && passwordRegex);
+  };
+  const verifyUser = () => {
+    const { email, password } = user;
+    const emailIsValid = verifyInputs(email, password);
+    if (emailIsValid) {
+      setValidInput(false);
+      return;
+    }
+    return setValidInput(true);
+  };
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleClick = () => {
+    const { dispatch, history } = props;
+    const { email } = user;
+    dispatch(saveUser(email));
+    history.push('/carteira');
+  };
+
+  useEffect(() => {
+    verifyUser();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
+  return (
+    <div>
+      <input
+        onChange={ handleChange }
+        type="text"
+        name="email"
+        data-testid="email-input"
+      />
+
+      <input
+        onChange={ handleChange }
+        type="password"
+        name="password"
+        data-testid="password-input"
+      />
+
+      <button
+        disabled={ validInput }
+        type="button"
+        onClick={ handleClick }
+      >
+        Entrar
+
+      </button>
+    </div>
+  );
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  user: state.user,
+});
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(Login);
